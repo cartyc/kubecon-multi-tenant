32a1278b2d0d73cd54f153c20feafce4a4329492
Chris Carty <chris.carty@gmail.com>
2020-05-08 22:18:07 -0400

    removed linkerd, cert-manager. Updated scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -20,14 +20,14 @@ echo "\n"
 # echo "/n"
 
 # Dev
-echo "Installing Dev Cluster"
-kind create cluster --name dev
-echo "Setting Dev Flux Agent"
-kubectl apply -k $(pwd)/install/ --dry-run -o yaml | sed 's/master/dev/'  | kubectl apply -f -
-kubectl delete -n flux-system secret flux-git-deploy
-kubectl create secret generic flux-git-deploy --from-file=identity=$HOME/.ssh/fluxcd -n flux-system
+# echo "Installing Dev Cluster"
+# kind create cluster --name dev
+# echo "Setting Dev Flux Agent"
+# kubectl apply -k $(pwd)/install/ --dry-run -o yaml | sed 's/master/dev/'  | kubectl apply -f -
+# kubectl delete -n flux-system secret flux-git-deploy
+# kubectl create secret generic flux-git-deploy --from-file=identity=$HOME/.ssh/fluxcd -n flux-system
 
-kubectx
+# kubectx
 
 
 # kubectx
--- scripts/change-keys.sh
+++ scripts/change-keys.sh
@@ -4,7 +4,7 @@
 {
     export NAMESPACE=gatekeeper-system
     kubectl delete secret flux-git-deploy -n $NAMESPACE
-    kubectl create secret generic flux-git-deploy --from-file=identity=/Users/work/.ssh/gatekeeper -n $NAMESPACE
+    kubectl create secret generic flux-git-deploy --from-file=identity=$HOME/.ssh/gatekeeper -n $NAMESPACE
     kubectl rollout restart deploy flux -n $NAMESPACE
 }
 
@@ -12,21 +12,21 @@
 {
     export NAMESPACE=falco-system
     kubectl delete secret flux-git-deploy -n $NAMESPACE
-    kubectl create secret generic flux-git-deploy --from-file=identity=/Users/work/.ssh/falco -n $NAMESPACE
+    kubectl create secret generic flux-git-deploy --from-file=identity=$HOME/.ssh/falco -n $NAMESPACE
 }
 
 # Demo App 
 {
     export NAMESPACE=demo-app
     kubectl delete secret flux-git-deploy -n $NAMESPACE
-    kubectl create secret generic flux-git-deploy --from-file=identity=/Users/work/.ssh/demo-app -n $NAMESPACE
+    kubectl create secret generic flux-git-deploy --from-file=identity=$HOME/.ssh/demo-app -n $NAMESPACE
 }
 
 # Linkerd
 {
     export NAMESPACE=linkerd
     kubectl delete secret flux-git-deploy -n $NAMESPACE
-    kubectl create secret generic flux-git-deploy --from-file=identity=/Users/work/.ssh/linkerd -n $NAMESPACE
+    kubectl create secret generic flux-git-deploy --from-file=identity=$HOME/.ssh/linkerd -n $NAMESPACE
 
     kubectl create secret tls \
     linkerd-trust-anchor \
